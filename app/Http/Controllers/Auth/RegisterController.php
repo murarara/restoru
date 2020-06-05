2d74c68eba20cfa16c534980ce3d4abc098a421c
murakami <murakami@arara.com>
2020-06-05 01:27:25 +0000

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,7 +68,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         $password = generatePassword();
-
+        
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
