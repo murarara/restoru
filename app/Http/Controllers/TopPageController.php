541968375e3399676875b16cb9e42f7c0c8429e2
aeri <aeri@gmail.com>
2020-06-05 07:05:40 +0000

    first login page

--- app/Http/Controllers/TopPageController.php
+++ app/Http/Controllers/TopPageController.php
@@ -15,12 +15,17 @@ class TopPageController extends Controller
     public function index(){
         $departments = Department::select('id', 'name')->get();
         $department_id_loop = $departments->pluck('name', 'id');
-        if (\Auth::check()) { // 認証済みの場合
-            if (\Auth::user()->flg_admin==1){
+        if (Auth::check()) { // 認証済みの場合
+            if (Auth::user()->flg_admin==1){
                 return view('auth.register', compact('department_id_loop'));
             }
             else{
-                return view('welcome');
+                
+                if(Auth::user()->flg_first_login==0){
+                    return view('users.reset_password');
+                } else {
+                    return view('welcome');
+                }
             }
         }
         return view('welcome');
@@ -34,13 +39,11 @@ class TopPageController extends Controller
         //dd($request->all());
         $this->validate($request, [
             'password' => ['required','string','min:8','confirmed'],
-            //'password_confirmation' => ['required','string','min:8','confirmed', 'same::password'],
         ]);
         
         $user = User::find(Auth::user()->id);
-        
         $user->password = Hash::make($request->password);
-        //$user->password_confirmation = Hash::make($request->password_confirmation);
+        $user->flg_first_login = 1;
 
         $user->save();
         
